caa29819be16727d5ccc0ab691d84e68862ede33
Kara Garvey <karagarvey123@gmail.com>
2019-02-27 13:29:17 -0500

    feat: 404 page added

--- client/components/index.js
+++ client/components/index.js
@@ -2,3 +2,4 @@ export { default as Spinner } from './Spinner';
 export { default as Search } from './Search';
 export { default as SearchResults } from './SearchResults';
 export { default as SingleSong } from './SingleSong';
+export { default as PageNotFound } from './PageNotFound';
--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 });
 
